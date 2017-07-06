23e1815334394695a0558c77ea252fde57ffaf22
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-07-06 09:06:40 +0300

    Create a get input function
    Created a function that gets the input from the user and then converts
     it to an integer

--- guess.py
+++ guess.py
@@ -7,3 +7,20 @@ def check_number(number):
         return False
     else:
         return True
+
+def get_input():
+    while True:
+        guess_input = ''
+        try:
+
+            guess_input = int(input())
+        except ValueError:
+            print("That is not a valid input")
+            guess_input = ''
+
+        if guess_input != '':
+            return guess_input
+            break
+
+
+print('I am thinking of a number can you guess what it is?')
